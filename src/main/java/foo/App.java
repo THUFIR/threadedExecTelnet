5da01b1c454be0a75bd7ee744019da00c0048348
thufir <thufir@dur.bounceme.net>
2020-05-16 15:44:24 -0700

    increment

---
+++ src/main/java/foo/App.java
@@ -0,0 +1,12 @@
+package foo;
+
+import java.util.logging.Logger;
+
+public class App {
+
+    private final static Logger log = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        log.info("watch files..");
+    }
+}
