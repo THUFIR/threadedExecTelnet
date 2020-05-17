da2f977f977e89cb6527e28af490a1baac1e9376
thufir <thufir@dur.bounceme.net>
2020-05-16 20:02:05 -0700

    increment

--- src/main/java/net/bounceme/dur/files/Parser.java
+++ src/main/java/net/bounceme/dur/files/Parser.java
@@ -1,7 +1,10 @@
 package net.bounceme.dur.files;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,7 +13,12 @@ public class Parser {
 
     private final static Logger log = Logger.getLogger(Parser.class.getName());
 
+    private Map<String, String> triggerMap = null;
+
     public Parser() {
+        triggerMap = new HashMap();
+        triggerMap.put(".*MAIN.*", "main menu found, press 1");
+        triggerMap.put("Press Return to continue:", "press return");
     }
 
     private void mainMenu(String line) {
@@ -30,6 +38,30 @@ public class Parser {
         if (matcher.matches()) {
             log.info(line);
             log.info("press return");
+        }
+    }
+
+    private void pullTrigger(String line, Map.Entry<String, String> entry) {
+        log.info(line);
+
+    }
+
+    private void triggers(String line) {
+        boolean match = false;
+        Pattern pattern = null;
+        Matcher matcher = null;
+
+        Iterator<Map.Entry<String, String>> entries = triggerMap.entrySet().iterator();
+
+        while (!match) {
+            while (entries.hasNext()) {
+                Map.Entry<String, String> entry = entries.next();
+                pattern = Pattern.compile(entry.getKey());
+                matcher = pattern.matcher(line);
+                if (matcher.matches()) {
+                    pullTrigger(line, entry);
+                }
+            }
         }
     }
 
