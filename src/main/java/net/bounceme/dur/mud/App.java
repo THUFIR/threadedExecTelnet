6ae3c812ab023b8ba9e6b10ddf764e232daafff3
thufir <thufir@dur.bounceme.net>
2020-05-19 01:09:51 -0700

    increment

--- src/main/java/net/bounceme/dur/mud/App.java
+++ src/main/java/net/bounceme/dur/mud/App.java
@@ -1,25 +1,18 @@
 package net.bounceme.dur.mud;
 
 import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.Logger;
-import net.bounceme.dur.exec.CommandEnum;
-import net.bounceme.dur.exec.RunExec;
 
 public class App {
 
     private final static Logger log = Logger.getLogger(App.class.getName());
+    private static Properties properties = new Properties();
 
     public static void main(String[] args) throws IOException {
-        Properties properties = null;
-        properties.loadFromXML(App.class.getResourceAsStream("/readPeopleText.xml"));
-        new RunExec().exec(CommandEnum.LS);
+        properties.loadFromXML(App.class.getResourceAsStream("/foo.xml"));
+        CommandMaker cm = new CommandMaker(properties);
+//        new RunExec().exec(CommandEnum.LS);
     }
 }
 
-
-/*
-    13	    private void run() throws BaseXException, IOException, Exception {
-    15	        Parser reader = new Parser(properties);
-    16	        List<Person> people = reader.iterate();
-
- */
